98f4385614383ccc046e3cc3c36e87ae6b5e6c3e
Princessjessy <ebubeprincess590@gmail.com>
2023-09-12 13:46:04 -0700

    javascript_objects_scopes_closures

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -6,9 +6,9 @@ const occurences = {};
 for (const userId in dict) { // looping through the object key
   const count = dict[userId]; // assigns the value to a variable
   if (count in occurences) { // if key exists
-    occurences[count].push(userId); 
+    occurences[count].push(userId); // access the object key, add to the value list
   } else {
-    occurences[count] = [userId]; 
+    occurences[count] = [userId]; // if no key, add the key to the object
   }
 }
 console.log(occurences);
